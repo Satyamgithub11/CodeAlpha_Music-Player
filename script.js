e4fccac857a6fefa8f0dc97c2e35780e8d598005
Satyamgithub11 <satyamkumar823@gmail.com>
2024-08-05 11:45:57 +0530

    add commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const playlist = [{
+    songName: "Ek Baar Nahi Ye Dil Sau Baar Hai Toota",
+    songImg: "https://i.ytimg.com/vi/waiB5mgjIo8/maxresdefault.jpg",
+    songSrc: "/music/Ek Baar Nahi Ye Dil Sau Baar Hai Toota_320(PagalWorld.com.so).mp3"
+}]
